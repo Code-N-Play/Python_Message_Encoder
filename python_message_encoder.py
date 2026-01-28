a49331a978cf888a69d2bb8439718bdfd58991a1
Code-N-Play <neerajkumarmaurya248@gmail.com>
2026-01-28 16:17:47 +0530

    Date 28 january 2026
    Python Message Encryptor

---
+++ python_message_encoder.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+
+
+print("......Welcom......\n")
+
+A= input ("Enter your message\n")
+Encryption = ""
+
+for char in A:
+    
+    #Capital letters
+    
+    if char=="A":
+        Encryption = Encryption + "😃"
+    elif char=="B":
+        Encryption = Encryption + "😁"
+    elif char=="C":
+        Encryption = Encryption + "😆"
+    elif char=="D":
+        Encryption = Encryption + "😅"    
+    elif char=="E":
+        Encryption = Encryption + "😂"
+    elif char=="F":
+        Encryption = Encryption + "🤣"
+    elif char=="G":
+        Encryption = Encryption + "😭"
+    elif char=="H":
+        Encryption = Encryption + "😚"
+    elif char=="I":
+        Encryption = Encryption + "🥰"
+    elif char=="J":
+        Encryption = Encryption + "🤩"
+    elif char=="K":
+        Encryption = Encryption + "🥳"
+    elif char=="L":
+        Encryption = Encryption + "🤠"
+    elif char=="M":
+        Encryption = Encryption + "🙃"
+    elif char=="N":
+        Encryption = Encryption + "🥲"
+    elif char=="O":
+        Encryption = Encryption + "🥸"
+    elif char=="P":
+        Encryption = Encryption + "☺️"
+    elif char=="Q":
+        Encryption = Encryption + "😏"
+    elif char=="R":
+        Encryption = Encryption + "🤤"
+    elif char=="S":
+        Encryption = Encryption + "😋"
+    elif char=="T":
+        Encryption = Encryption + "🍎"
+    elif char=="U":
+        Encryption = Encryption + "🍊"
+    elif char=="V":
+        Encryption = Encryption + "🥺"
+    elif char=="W":
+        Encryption = Encryption + "😬"
+    elif char=="X":
+        Encryption = Encryption + "😑"
+    elif char=="Y":
+        Encryption = Encryption + "😷"
+    elif char=="Z":
+        Encryption = Encryption + "🤐"
+     
+     
+    #Small Letters
+    
+    elif char=="a":
+        Encryption = Encryption + "🤔"
+    elif char=="b":
+        Encryption = Encryption + "🤭"
+    elif char=="c":
+        Encryption = Encryption + "🥱"
+    elif char=="d":
+        Encryption = Encryption + "🤗"    
+    elif char=="e":
+        Encryption = Encryption + "🤓"
+    elif char=="f":
+        Encryption = Encryption + "😱"
+    elif char=="g":
+        Encryption = Encryption + "🤨"
+    elif char=="h":
+        Encryption = Encryption + "🧐"
+    elif char=="i":
+        Encryption = Encryption + "😒"
+    elif char=="j":
+        Encryption = Encryption + "🙄"
+    elif char=="k":
+        Encryption = Encryption + "😤"
+    elif char=="l":
+        Encryption = Encryption + "😡"
+    elif char=="m":
+        Encryption = Encryption + "🤬"
+    elif char=="n":
+        Encryption = Encryption + "😓"
+    elif char=="o":
+        Encryption = Encryption + "😟"
+    elif char=="p":
+        Encryption = Encryption + "😢"
+    elif char=="q":
+        Encryption = Encryption + "☹️"
+    elif char=="r":
+        Encryption = Encryption + "😨"
+    elif char=="s":
+        Encryption = Encryption + "😯"
+    elif char=="t":
+        Encryption = Encryption + "🔞"
+    elif char=="u":
+        Encryption = Encryption + "⛔"
+    elif char=="v":
+        Encryption = Encryption + "😖"
+    elif char=="w":
+        Encryption = Encryption + "😣"
+    elif char=="x":
+        Encryption = Encryption + "😵"
+    elif char=="y":
+        Encryption = Encryption + "🤧"
+    elif char=="z":
+        Encryption = Encryption + "🤕"
+     
+    #Numbers
+             
+    elif char=="0":
+        Encryption = Encryption + "⏱️"
+    elif char=="1":
+        Encryption = Encryption + "🕐"
+    elif char=="2":
+        Encryption = Encryption + "🕑"
+    elif char=="3":
+        Encryption = Encryption + "🕒"
+    elif char=="4":
+        Encryption = Encryption + "🕓"
+    elif char=="5":
+        Encryption = Encryption + "🕔"
+    elif char=="6":
+        Encryption = Encryption + "🕕"
+    elif char=="7":
+        Encryption = Encryption + "🕖"
+    elif char=="8":
+        Encryption = Encryption + "🕗"
+    elif char=="9":
+        Encryption = Encryption + "🕘"
+        
+    else:
+        Encryption = Encryption +"⚪"  
+        
+print ("\nYour Encrypted Message is : ")                        
+print (Encryption)
+  
+print("\n\n ......Tanks for Using......")    
